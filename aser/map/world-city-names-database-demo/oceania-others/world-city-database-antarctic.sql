93e3eb22051f784389b4bb8b9325b840fb668f88
Yusei Hara <yu-star@outlook.com>
2021-08-19 19:13:49 +0900

    aser data

---
+++ aser/map/world-city-names-database-demo/oceania-others/world-city-database-antarctic.sql
@@ -0,0 +1,11 @@
+SET autocommit=1;
+
+#
+# Dumping data for table 'Cities' - Continent: Antarctic Region
+#
+
+INSERT INTO `Cities` VALUES (904294, 'GS', '00', 'grytviken', 'Grytviken', -54.2766667, -36.5116667);
+INSERT INTO `Cities` VALUES (904295, 'GS', '00', 'stromness', 'Stromness', -54.1561111, -36.7161111);
+INSERT INTO `Cities` VALUES (2353594, 'TF', '00', 'port-aux-francais', 'Port-aux-Français', -49.35, 70.2166667);
+# 3 records
+
